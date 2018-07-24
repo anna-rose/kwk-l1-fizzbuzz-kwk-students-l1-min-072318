c2be95c0cbcf421c1ab9e8e499f340b39722d6b2
anna-rose <917566@isd624.org>
2018-07-24 19:03:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(x)
   if x % 3 == 0 && x % 5 == 0
-    return "FizzBuzz"
+    puts "FizzBuzz"
   elsif x % 3 == 0 
-    return "Fizz"
+    puts "Fizz"
   elsif x % 5 == 0 
-    return "Buzz"
+    puts "Buzz"
   else 
     puts "nil"
   end 
